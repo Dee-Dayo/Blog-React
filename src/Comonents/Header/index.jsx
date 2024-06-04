240b5cc97361c96b56480d540f17d6835809e729
Dee-dayo <darhyor2050@gmail.com>
2024-06-04 09:33:33 +0100

    added hamburger to the mobile responsiveness

--- src/Comonents/Header/index.jsx
+++ src/Comonents/Header/index.jsx
@@ -1,26 +1,57 @@
 import style from "./index.module.css";
 import logo from "../../assets/deeLogo.png";
-import React from "react";
+import React, {useState} from "react";
+import Hamburger from "hamburger-react"
 import FilledButton from "../FilledButton";
 
 const Header = () => {
+    const isMobile = window.innerWidth <= 1080;
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
-        <div className={style.nav}>
-            <div className={style.image}>
-                <img src={logo} alt="logo"/>
-            </div>
+        <>
+            <div className={style.nav}>
+                <div className={style.image}>
+                    <img src={logo} alt="logo"/>
+                </div>
 
-            <div className={style.midSection}>
-                <p><a href="/home" style={{textDecoration: "none", color: "#4d4d4d"}}> Home</a></p>
-                <p>About Us</p>
-                <p><a href="/service" style={{textDecoration: "none", color: "#4d4d4d"}}>Our Service</a></p>
-            </div>
+                {isMobile ? (
+                    <>
+                        <div className={style.hamburger}>
+                            <Hamburger toggled={isOpen} toggle={setIsOpen} color="#5cb30b"/>
+                        </div>
+                        {isOpen && (
+                            <div className={style.mobileMenu}>
+                                <div className={style.midSection}>
+                                    <p><a href="/home" style={{textDecoration: "none", color: "#4d4d4d"}}> Home</a></p>
+                                    <p>About Us</p>
+                                    <p><a href="/service" style={{textDecoration: "none", color: "#4d4d4d"}}>Our
+                                        Service</a></p>
+                                </div>
 
-            <div className={style.btn}>
-                <p className={style.loginBtn}><a href="/login">Login</a></p>
-                <FilledButton name={"Sign Up"}/>
+                                <div className={style.btn}>
+                                    <p className={style.loginBtn}><a href="/login">Login</a></p>
+                                    <FilledButton name={"Sign Up"}/>
+                                </div>
+                            </div>
+                        )}
+                    </>
+                ) : (
+                    <>
+                        <div className={style.midSection}>
+                            <p><a href="/home" style={{textDecoration: "none", color: "#4d4d4d"}}> Home</a></p>
+                            <p>About Us</p>
+                            <p><a href="/service" style={{textDecoration: "none", color: "#4d4d4d"}}>Our Service</a></p>
+                        </div>
+
+                        <div className={style.btn}>
+                            <p className={style.loginBtn}><a href="/login">Login</a></p>
+                            <FilledButton name={"Sign Up"}/>
+                        </div>
+                    </>
+                )}
             </div>
-        </div>
+        </>
     )
 }
 
